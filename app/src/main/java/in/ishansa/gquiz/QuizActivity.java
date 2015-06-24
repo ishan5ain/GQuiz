6fc9c428d9476c9b4e2f691106fcf742f0e07daa
Ishan Sain <ishansain27@gmail.com>
2015-06-24 18:15:27 +0530

    App now runs in full screen. Hides Notification bar.

--- app/src/main/java/in/ishansa/gquiz/QuizActivity.java
+++ app/src/main/java/in/ishansa/gquiz/QuizActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,6 +50,10 @@ public class QuizActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
