69b195ba9502fe607758a632359fab9e8331a640
Ishan Sain <ishansain27@gmail.com>
2015-06-24 17:03:29 +0530

    Added Splash Screen: a deliberate 3s of splash showing my name. I deserve this.

---
+++ app/src/main/java/in/ishansa/gquiz/splash.java
@@ -0,0 +1,43 @@
+package in.ishansa.gquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by insain on 24-06-2015.
+ */
+public class splash extends Activity{
+    private static int splashInterval = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(splash.this, QuizActivity.class);
+                startActivity(i);
+
+                this.finish();
+            }
+
+            private void finish() {
+
+            }
+        }, splashInterval);
+
+
+    }
+
+}
