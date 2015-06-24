1a5bad0e95199beb05181002870fb62819ce3b37
Ishan Sain <ishansain27@gmail.com>
2015-06-24 12:47:44 +0530

    first commit GQuiz

---
+++ app/src/main/java/in/ishansa/gquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package in.ishansa.gquiz;
+
+/**
+ * Created by insain on 23-06-2015.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+
+    }
+
+    public int getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean ismTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setmTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
